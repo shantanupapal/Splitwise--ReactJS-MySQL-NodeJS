c5ad0fc9459692122b92af40691c89e2fdf5d1f8
shantanupapal <40557046+shantanupapal@users.noreply.github.com>
2021-03-19 05:10:25 +0530

    show all group balances

--- backend/routes/getgroupbalancedetails.js
+++ backend/routes/getgroupbalancedetails.js
@@ -5,10 +5,13 @@ const pool = require("../pool");
 router.post("/", (req, res) => {
     const group_id = parseInt(req.body.group_id);
     console.log("Group_id: ", group_id);
+    const payers = [];
+    const borrowers = [];
+    const all_balances = [];
     pool.query(
         "SELECT u.name,paid_by, SUM(amount) AS amount FROM splitwise.expenses e JOIN splitwise.users u ON e.paid_by = u.user_id WHERE group_id=? group by paid_by ORDER BY paid_by;",
         [group_id],
-        (err, result) => {
+        (err, results) => {
             if (err) {
                 console.log("Error: ", err);
                 res.writeHead(500, {
@@ -18,16 +21,19 @@ router.post("/", (req, res) => {
 
                 // res.send({ err: err });
             }
-            if (result.length === 0) {
-                //check only for liables group
-            }
-            if (result.length > 0 || result.length === 0) {
-                console.log("PAYERS: ", result);
+            // if (result.length === 0) {
+            //     //check only for liables group
+            // }
+            if (results.length >= 0) {
+                // console.log("PAYERS: ", results);
+                results.forEach((result) => {
+                    payers.push([result.name, result.paid_by, result.amount]);
+                });
 
                 pool.query(
-                    "SELECT u.name, liable, SUM(amount) FROM splitwise.expenses e JOIN splitwise.users u ON e.liable = u.user_id WHERE group_id=? group by liable ORDER BY liable;",
+                    "SELECT u.name, liable, SUM(amount) AS amount FROM splitwise.expenses e JOIN splitwise.users u ON e.liable = u.user_id WHERE group_id=? group by liable ORDER BY liable;",
                     [group_id],
-                    (err, result) => {
+                    (err, results) => {
                         if (err) {
                             console.log("Error: ", err);
                             res.writeHead(500, {
@@ -37,11 +43,36 @@ router.post("/", (req, res) => {
 
                             // res.send({ err: err });
                         }
-                        if (result.length === 0) {
-                            console.log("No users found");
-                        }
-                        if (result.length > 0) {
-                            console.log("BORROWERS: ", result);
+                        // if (result.length === 0) {
+                        //     console.log("No users found");
+                        // }
+                        if (results.length >= 0) {
+                            // console.log("BORROWERS: ", result);
+                            results.forEach((result) => {
+                                borrowers.push([
+                                    result.name,
+                                    result.liable,
+                                    result.amount,
+                                ]);
+                            });
+                            console.log("BORROWERS: ", borrowers);
+                            console.log("PAYERS: ", payers);
+
+                            payers.forEach((payer) => {
+                                borrowers.forEach((borrower) => {
+                                    if (payer[1] === borrower[1]) {
+                                        let balance = payer[2] - borrower[2];
+                                        all_balances.push([
+                                            payer[0],
+                                            payer[1],
+                                            balance,
+                                        ]);
+                                    }
+                                });
+                            });
+
+                            console.log(all_balances);
+                            res.status(200).send(all_balances);
                         }
                     }
                 );
